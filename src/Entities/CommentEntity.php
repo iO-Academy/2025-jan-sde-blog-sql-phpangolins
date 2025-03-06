c885e4f570078c2fa961c7ddfaca2b3fd9f73e2f
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-06 14:46:37 +0000

    added getters for comment entity

--- src/Entities/CommentEntity.php
+++ src/Entities/CommentEntity.php
@@ -6,7 +6,34 @@ class CommentEntity
     private int $id;
     private string $comment;
     private int $post_id;
-    private int $user_id;
+    private string $username;
+    private string $date;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    public function getPostId(): int
+    {
+        return $this->post_id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
 
 
 }
