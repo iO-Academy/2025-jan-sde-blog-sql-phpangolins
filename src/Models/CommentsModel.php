85e9e21f8871177cfdca47cafb07769769a3e4a3
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-06 14:26:09 +0000

    created CommentsModel class. Has a function that inserts values to the comments table and returns a boolean

--- src/Models/CommentsModel.php
+++ src/Models/CommentsModel.php
@@ -1,1 +1,22 @@
 <?php
+declare(strict_types=1);
+
+class CommentsModel
+{
+    public PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function insertCommentIntoTable(string $comment, int $post_id, int $user_id) : bool
+    {
+        $query = $this->db->prepare("INSERT INTO `comments` (`comment`, `post_id`, `user_id`, `date`)
+                                            VALUES (:comment, :post_id, :user_id, :date);");
+        if ($query->execute([':comment' => $comment, ':post_id' => $post_id, ':user_id' => $user_id, ':date' => date("Y-m-d")])) {
+            return true;
+        }
+        return false;
+    }
+}
