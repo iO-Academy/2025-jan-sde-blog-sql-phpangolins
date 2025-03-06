0f6191d8d1fe5642c155a8dd0e8cd403dc05b1a8
Chris Hubley <chrisjhubley@gmail.com>
2025-03-06 10:59:10 +0000

    completed story 3

--- register.php
+++ register.php
@@ -1,55 +1,32 @@
 <?php
 declare(strict_types=1);
-session_start();
+
 require_once 'src/Services/NavBarService.php';
 require_once 'src/Services/DatabaseConnectionServices.php';
 require_once 'src/models/UsersModel.php';
 require_once 'src/Services/RegisterService.php';
-
-//$displayError = false;
+session_start();
 
 if (isset($_POST['submitted'])) {
-
     $db = DatabaseConnectionServices::connect();
     $user = new UsersModel($db);
 
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-//    var_dump( RegisterService::validateUsername($username));
-//    var_dump( RegisterService::validateEmail($email));
-//    var_dump( RegisterService::validatePassword($password));
-
-    var_dump($user->registrationValidationCheckUsername($username));
-    var_dump($user->registrationValidationCheckEmail($email));
-
     if (
-            RegisterService::validateUsername($username)
+        RegisterService::validateUsername($username)
         && RegisterService::validateEmail($email)
         && RegisterService::validatePassword($password)
         && $user->registrationValidationCheckUsername($username)
-        && $user->registrationValidationCheckEmail($email))
-    {
-        $user->addUser($username, $email, $password);
+        && $user->registrationValidationCheckEmail($email)) {
+            $user->addUser($username, $email, $password);
+            $_SESSION ['loggedIn'] = true;
+            $_SESSION ['username'] = $username;
+            header('Location:index.php');
     }
-    else
-    {
-        echo "<p>Didn't work</p>";
-    }
-
-
-//    if ($user -> checkUser($email, $password) !== false) {
-//        $displayError = false;
-//        $_SESSION ['loggedIn'] = true;
-//        header('Location:index.php');
-//    }
-//    $displayError = true;
 }
-
-var_dump($_POST);
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -69,18 +46,36 @@ var_dump($_POST);
         <div class="mb-5">
             <label class="mb-3 block" for="username">Username:</label>
             <input class="w-full px-3 py-2 text-lg" type="text" id="username" name="username" />
+            <?php
+                if (isset($_POST['submitted']) && $user->registrationValidationCheckUsername($username) === false){
+                    echo "<p class='text-red-600'>Username already taken</p>";
+                }
+                if (isset($_POST['submitted']) && RegisterService::validateUsername($username) ===false){
+                    echo "<p class='text-red-600'>username must be between 3 and 25 characters</p>";
+                }
+        ?>
         </div>
-
         <div class="mb-5">
             <label class="mb-3 block" for="email">Email:</label>
-            <input class="w-full px-3 py-2 text-lg" type="email" id="email" name="email" />
+            <input class="w-full px-3 py-2 text-lg" type="text" id="email" name="email" />
+            <?php
+            if (isset($_POST['submitted']) && RegisterService::validateEmail($email) === false){
+                echo "<p class='text-red-600'>Must be a valid email</p>";
+            }
+            if (isset($_POST['submitted']) && $user->registrationValidationCheckEmail($email) ===false){
+                echo "<p class='text-red-600'>Email is already registered</p>";
+            }
+            ?>
         </div>
-
         <div class="mb-5">
             <label class="mb-3 block" for="password">Password:</label>
             <input class="w-full px-3 py-2 text-lg" type="password" id="password" name="password" />
+            <?php
+            if (isset($_POST['submitted']) && RegisterService::validatePassword($password) === false){
+                echo '<p class="text-red-600">Password must be over 8 characters and not contain the word "password"</p>';
+            }
+            ?>
         </div>
-
         <input class="px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" type="submit" value="Register" name="submitted" />
     </form>
 </body>
