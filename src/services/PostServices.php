81ddf2a64f867de990c9e14b70d95c269e75f7c4
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-06 11:20:08 +0000

    inside displaySinglePost - using getters of PostEntity to display correct info. Added logic for author name. also added '...' into displaySingleHomepage function as we noticed it was missing

--- src/services/PostServices.php
+++ src/services/PostServices.php
@@ -11,7 +11,7 @@ class PostServices
         $outcome .= '<div class="flex justify-between items-center flex-col md:flex-row mb-4">';
         $outcome .= "<h2 class='text-4xl'>" . $post->getTitle()."</h2></div>";
         $outcome .= "<p class='text-2xl mb-2'>".date_format($date,"d/m/y")." - By ".$post->getAuthor()."</p>";
-        $outcome .= "<p>".substr($post->getContent(), 0, 100)."</p>";
+        $outcome .= "<p>".substr($post->getContent(), 0, 100)."...</p>";
         $outcome .= '<div class="flex justify-center">';
         $outcome .= '<a class="px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" href="singlePostPage.php?id=' . $post->getId() . '">View Post</a>';
         $outcome .= '</div>';
@@ -34,13 +34,22 @@ class PostServices
 
     static public function displaySinglePost(PostEntity $post): string
     {
+        $date = date_create($post->getDateTime());
+
         $outcome = "<section class='container md:w-1/2 mx-auto'>";
         $outcome .= "<article class='p-8 border border-solid rounded-md'>";
         $outcome .= "<div class='flex justify-between items-center flex-col md:flex-row mb-4'>";
-        $outcome .= "<h2 class='text-4xl'>Example title</h2>";
+        $outcome .= "<h2 class='text-4xl'>" . $post->getTitle() . "</h2>";
         $outcome .= "</div>";
-        $outcome .= "<p class='text-2xl mb-10'>01/01/2024 - by bob</p>";
-        $outcome .= "<p>CONTENT</p>";
+        $outcome .= "<p class='text-2xl mb-10'>" . date_format($date,"d/m/y");
+
+        if ($post->getAuthor() == '') {
+            $outcome .= " - By Anonymous</p>";
+        } else {
+            $outcome .= " - By " . $post->getAuthor() . "</p>";
+        }
+
+        $outcome .= "<p>" . $post->getContent() . "</p>";
         $outcome .= "<a class='px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm' href='index.php'>View all posts</a>";
         $outcome .= "</article>";
         $outcome .= "</section>";
