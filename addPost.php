723c05cf733007e8d7c80991629357da56cf88ae
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-07 10:40:12 +0000

    deleted unused model

--- addPost.php
+++ addPost.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 require_once 'src/Services/NavBarService.php';
-require_once 'src/Models/AddPostsModel.php';
 require_once 'src/Services/DatabaseConnectionServices.php';
 require_once 'src/Services/AddPostsServices.php';
 require_once 'src/Services/PostServices.php';
