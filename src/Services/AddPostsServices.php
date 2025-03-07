9df73e9e1d6bc41985f6ef755260aa3977c5669d
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-07 09:32:46 +0000

    moved tests over to relevant file and corrected

--- src/Services/AddPostsServices.php
+++ src/Services/AddPostsServices.php
@@ -1,22 +1,22 @@
 <?php
-
-declare(strict_types=1);
-
-class AddPostsServices
-{
-//    static public function validTitle($titleLength): bool
-//    {
-//        if ($titleLength > 30) {
-//            return false;
-//        }
-//        return true;
-//    }
-
-//    static public function validContent($contentLength): bool
-//    {
-//        if ($contentLength < 50 || $contentLength > 1001) {
-//            return false;
-//        }
-//        return true;
-//    }
-}
+//
+//declare(strict_types=1);
+//
+//class AddPostsServices
+//{
+////    static public function validTitle($titleLength): bool
+////    {
+////        if ($titleLength > 30) {
+////            return false;
+////        }
+////        return true;
+////    }
+//
+////    static public function validContent($contentLength): bool
+////    {
+////        if ($contentLength < 50 || $contentLength > 1001) {
+////            return false;
+////        }
+////        return true;
+////    }
+//}
--- tests/PostServicesTest.php
+++ tests/PostServicesTest.php
@@ -44,4 +44,58 @@ class PostServicesTest extends TestCase{
          $this->assertStringContainsString('Title 1', $method);
          $this->assertStringContainsString('Title 2', $method);
        }
+
+    public function test_validTitle_lengthUnder30(): void
+    {
+        $title = 'titleLength';
+        $titleLength = strlen($title);
+        $actual = PostServices::validTitle($titleLength);
+
+        $this ->assertTrue($actual);
+    }
+
+    public function test_validTitle_lengthOver30(): void
+    {
+        $title = 'titleLengthIsThisTextGoingToBeOver30WhoKnows';
+        $titleLength = strlen($title);
+        $actual = PostServices::validTitle($titleLength);
+
+        $this ->assertFalse($actual);
+    }
+
+    public function test_validContent_lengthUnder50(): void
+    {
+        $content = 'contentLength';
+        $contentLength = strlen($content);
+        $actual = PostServices::validContent($contentLength);
+
+        $this ->assertFalse($actual);
+    }
+
+    public function test_validContent_lengthOver1001(): void
+    {
+        $content = 'ContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPants';
+        $contentLength = strlen($content);
+        $actual = PostServices::validContent($contentLength);
+
+        $this ->assertFalse($actual);
+    }
+
+    public function test_validContent_lengthOver50(): void
+    {
+        $content = 'ContentLengthContentLengthContentLengthContentLengthContentLengthContentLength';
+        $contentLength = strlen($content);
+        $actual = PostServices::validContent($contentLength);
+
+        $this ->assertTrue($actual);
+    }
+
+    public function test_validContent_lengthUnder1001(): void
+    {
+        $content = 'ContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPantsContentLengthIsGoingToBeTheLongestLineYouHaveEverSeenEverInYourWholeEntireLifeHoldOntoYourHatsAndYourPants';
+        $contentLength = strlen($content);
+        $actual = PostServices::validContent($contentLength);
+
+        $this ->assertTrue($actual);
+    }
 }
