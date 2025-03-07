ce0f88344c89823481b07e81b740689c30e736fc
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-07 10:55:30 +0000

    unsure how to go forward

--- src/Services/LikesDislikesService.php
+++ src/Services/LikesDislikesService.php
@@ -8,8 +8,8 @@ class LikesDislikesService
         $output = '';
 
         if ($loggedIn) {
-            $output .= '<a class="px-3 py-2 mt-4 text-lg bg-green-300 hover:bg-green-400 hover:text-white transition inline-block rounded-sm" href="addLike.php">Like</a>';
-            $output .= '<a class="px-3 py-2 mt-4 text-lg bg-red-300 hover:bg-red-400 hover:text-white transition inline-block rounded-sm" href="addDislike.php">Dislike</a>';
+            $output .= '<a class="px-3 py-2 mt-4 text-lg bg-green-300 hover:bg-green-400 hover:text-white transition inline-block rounded-sm" href="userLikePost.php">Like</a>';
+            $output .= '<a class="px-3 py-2 mt-4 text-lg bg-red-300 hover:bg-red-400 hover:text-white transition inline-block rounded-sm" href="userDislikePost.php">Dislike</a>';
         } else {
             $output .= '<a class="px-3 py-2 mt-4 text-lg bg-green-300 hover:bg-green-400 hover:text-white transition inline-block rounded-sm" href="login.php">Like</a>';
             $output .= '<a class="px-3 py-2 mt-4 text-lg bg-red-300 hover:bg-red-400 hover:text-white transition inline-block rounded-sm" href="login.php">Dislike</a>';
